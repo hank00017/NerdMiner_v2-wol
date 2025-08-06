84e7e79853f4835bc80bdbe12a21a033b0d53949
hank00017 <hank00017@gmail.com>
2025-08-06 22:53:37 +0800

    Add send magic packet function

--- src/nettools/wol.cpp
+++ src/nettools/wol.cpp
@@ -1,10 +1,33 @@
 #include <WiFiUdp.h>
 #include <WebServer.h>
 #include <ESP32Ping.h>
-#include "wol.h"
 #include <HTTPClient.h>
+#include "wol.h"
 
-// List of WOL devices: {"Name", "MAC", "Broadcast IP", Port}
+
+// List of WOL devices: {"Name", "MAC", "Broadcast IP"}
 wolDevice devices[] = {
-  {"Device1", "AA:BB:CC:DD:EE:FF", "192.168.1.255", 9 },
+  {"Device1", "AA:BB:CC:DD:EE:FF", "192.168.1.255"},
 };
+
+// UDP instance for sending WOL packets
+WiFiUDP udp;
+WebServer server(80);
+
+void sendWOLPacket(const byte* macAddress) {
+  byte magicPacket[102];
+  // create the magic packet
+  for (int i = 0; i < 6; i++) {
+    magicPacket[i] = 0xFF;
+  }
+  for (int i = 1; i <= 16; i++) {
+    for (int j = 0; j < 6; j++) {
+      magicPacket[i * 6 + j] = macAddress[j];
+    }
+  }
+  // send the magic packet
+  udp.beginPacket("255.255.255.255", wolPort);
+  udp.write(magicPacket, sizeof(magicPacket));
+  udp.endPacket();
+}
+
--- src/nettools/wol.h
+++ src/nettools/wol.h
@@ -6,12 +6,14 @@
 #include <ESP32Ping.h>
 #include <HTTPClient.h>
 
+#define wolPort 9  // Default WOL port
 struct wolDevice {
   const char* name;
   const char* macAddress;    // MAC address
   const char* ipAddress;     // IP address
-  uint16_t port;             // Usually 9 or 7
 };
 
+
+void sendWOLPacket(const byte* macAddress);
 
 #endif // WOL_H
