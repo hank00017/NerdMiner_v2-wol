a98dfa8f1ceb49e3563235bae04af4b075fa1bb2
hank00017 <hank00017@gmail.com>
2025-08-08 02:17:56 +0800

    feat: add dynamic multi-device buttons to WOL webpage

--- src/nettools/wol.cpp
+++ src/nettools/wol.cpp
@@ -3,11 +3,15 @@
 #include <ESP32Ping.h>
 #include <HTTPClient.h>
 #include "wol.h"
+#include "wolHTML.h"
 
 
 // List of WOL devices: {"Name", "MAC", "Broadcast IP"}
 wolDevice devices[] = {
   {"Device1", "AA:BB:CC:DD:EE:FF", "192.168.1.255"},
+  {"Device2", "AA:BB:CC:DD:EE:FF", "192.168.1.255"},
+  {"Device3", "AA:BB:CC:DD:EE:FF", "192.168.1.255"},
+  {"Device4", "AA:BB:CC:DD:EE:FF", "192.168.1.255"},
 };
 
 // UDP instance for sending WOL packets
@@ -31,9 +35,33 @@ void sendWOLPacket(const byte* macAddress) {
   udp.endPacket();
 }
 
+// 計算設備數量
+const int deviceCount = sizeof(devices) / sizeof(devices[0]);
+
 //create a webpage 
 void handleRoot() {
-    String html = "<html><body><h1>WOL</h1>";
+    String html = String(WOL_HTML_PAGE);
+
+        // 生成設備按鈕
+    String deviceButtons = "";
+
+    for (int i = 0; i < deviceCount; i++) {
+        
+        deviceButtons += "<div style='margin: 15px 0;'>";
+        deviceButtons += "<button style=\"width: 100%; height: 75px; cursor: pointer;\" "
+                 "onclick=\"window.location.href='/wake?device=" + String(i) + "'\">"
+                 "Wake " + String(devices[i].name) +
+                 "</button>";
+        deviceButtons += "</div>";
+    }
+    
+    // 如果沒有設備，顯示提示訊息
+    if (deviceCount == 0) {
+        deviceButtons = "<div style='text-align:center;color:#666;'>No devices configured</div>";
+    }
+
+    // 替換佔位符
+    html.replace("{{DEVICE_BUTTONS}}", deviceButtons);
     server.send(200, "text/html", html);
 }
 
